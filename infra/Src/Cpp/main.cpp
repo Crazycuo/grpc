39f3ede5c66311957af515cd66630ddd70ffdf6e
Crazycuo <740094202@qq.com>
2018-09-09 00:26:24 +0800

    Revert "Revert "Stage 5:""
    This reverts commit 7fb5113a1d40c9dabc0655e7f8ca5fa53804c135.

---
+++ infra/Src/Cpp/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * it's an example by using makefile in Windows by atom_make
+ *
+ * it's better to type "make" in the DOS rather than using F6
+ */
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  std::cout << "this is a test of Atom Makefile" << '\n';
+  return 0;
+}
